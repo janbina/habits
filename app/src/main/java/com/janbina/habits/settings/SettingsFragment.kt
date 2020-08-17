bf57b08e7289604788baac3cd7bd0ac50ef130e6
Jan Bína <binajohny@gmail.com>
2020-08-17 14:13:49 +0200

    Simple firebase auth

--- app/src/main/java/com/janbina/habits/settings/SettingsFragment.kt
+++ app/src/main/java/com/janbina/habits/settings/SettingsFragment.kt
@@ -1,9 +1,13 @@
 package com.janbina.habits.settings
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.janbina.habits.DateFormatters
 import com.janbina.habits.HomeViewModel
+import com.janbina.habits.MainActivity
 import com.janbina.habits.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -12,5 +16,13 @@ import javax.inject.Inject
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
     private val viewModel: SettingsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.loginButton).setOnClickListener {
+            (activity as MainActivity).login()
+        }
+    }
 
 }
