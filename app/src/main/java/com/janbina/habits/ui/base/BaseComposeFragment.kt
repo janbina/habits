93151c6d0e5c9448e142bf5a4732f856850762c8
Jan Bína <binajohny@gmail.com>
2020-09-02 00:02:42 +0200

    Better base fragment

--- app/src/main/java/com/janbina/habits/ui/base/BaseComposeFragment.kt
+++ app/src/main/java/com/janbina/habits/ui/base/BaseComposeFragment.kt
@@ -1,13 +1,35 @@
 package com.janbina.habits.ui.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.airbnb.mvrx.BaseMvRxFragment
 import com.airbnb.mvrx.MvRxView
+import com.janbina.habits.ui.detail.HabitDetailScreen
 import com.janbina.habits.ui.viewevent.NavigationEvent
 import kotlinx.coroutines.flow.launchIn
 
-abstract class BaseComposeFragment : BaseMvRxFragment(), MvRxView {
+abstract class BaseComposeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent { content() }
+        }
+    }
+
+    @Composable
+    protected abstract fun content()
 
     protected fun BaseComposeViewModel<*>.handleNavigationEvents() {
         onEachEvent<NavigationEvent> {
--- app/src/main/java/com/janbina/habits/ui/detail/HabitDetailFragment.kt
+++ app/src/main/java/com/janbina/habits/ui/detail/HabitDetailFragment.kt
@@ -2,13 +2,11 @@ package com.janbina.habits.ui.detail
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.compose.ui.platform.ComposeView
+import androidx.compose.runtime.Composable
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.janbina.habits.databinding.ItemCalendarDayDetailBinding
 import com.janbina.habits.helpers.DateFormatters
 import com.janbina.habits.ui.base.BaseComposeFragment
@@ -22,28 +20,17 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class HabitDetailFragment : BaseComposeFragment() {
 
-    private val viewModel: HabitDetailViewModel by viewModels()
-
     @Inject
     lateinit var dateFormatters: DateFormatters
 
-    override fun invalidate() {}
+    private val viewModel: HabitDetailViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                HabitDetailScreen(
-                    dateFormatters = dateFormatters,
-                    navController = findNavController(),
-                    binder = dayBinder
-                )
-            }
-        }
-    }
+    @Composable
+    override fun content() = HabitDetailScreen(
+        dateFormatters = dateFormatters,
+        navController = findNavController(),
+        binder = dayBinder
+    )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
